6438500eb31f4e8c3eaf9e9eef712b6c18abe9ae
Rui Freitas <rui4825@gmail.com>
2017-07-03 14:01:43 +0100

    error string

--- EvaluationSystem-cli/src/app/components/dashboard/class/groups/group-view/group-view.component.ts
+++ EvaluationSystem-cli/src/app/components/dashboard/class/groups/group-view/group-view.component.ts
@@ -80,16 +80,16 @@ export class GroupViewComponent implements OnInit, AfterViewInit  {
 
   ClickButton(): void {
     this.popup1.options = {
-      header: "" ,
-      color: "red", // red, blue....
+      header: '' ,
+      color: 'red', // red, blue....
       widthProsentage: 40, // The with of the popou measured by browser width
       animationDuration: 1, // in seconds, 0 = no animation
       showButtons: true, // You can hide this in case you want to use custom buttons
-      confirmBtnContent: "OK", // The text on your confirm button
-      cancleBtnContent: "Cancel", // the text on your cancel button
-      confirmBtnClass: "btn btn-default", // your class for styling the confirm button
-      cancleBtnClass: "btn btn-default", // you class for styling the cancel button
-      animation: "fadeInDown" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
+      confirmBtnContent: 'OK', // The text on your confirm button
+      cancleBtnContent: 'Cancel', // the text on your cancel button
+      confirmBtnClass: 'btn btn-default', // your class for styling the confirm button
+      cancleBtnClass: 'btn btn-default', // you class for styling the cancel button
+      animation: 'fadeInDown' // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
     };
 
     this.popup1.show(this.popup1.options);
